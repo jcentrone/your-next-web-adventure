9ff59a6981c4f0a9c1f24ff2c3e0e173f8fd73b3
Joe Centrone <joecentrone@gmail.com>
2025-08-19 19:38:01 -0400

    Added functionality to download wind mit pdf

---
+++ src/lib/windMitigationFieldMap.ts
@@ -0,0 +1,111 @@
+// src/utils/windMitigationFieldMap.ts
+export const WIND_MITIGATION_FIELD_MAP: Record<string, string> = {
+  // --- Owner Info ---
+  clientName: "Owner Name",
+  contactPerson: "Contact Person",
+  address: "Address",
+  city: "City",
+  zipCode: "Zip",
+  phoneHome: "Home Phone",
+  phoneWork: "Work Phone",
+  phoneCell: "Cell Phone",
+  email: "Email",
+  insuranceCompany: "Insurance Company",
+  policyNumber: "Policy",
+
+  // --- Inspection Info ---
+  inspectionDate: "Inspection Date",
+  yearBuilt: "Year of Home",
+  stories: "# of Stories",
+
+   // --- Roof Covering Types ---
+  roofTypeAsphalt: "1 AsphaltFiberglass Shingle",
+  roofTypeTile: "2 ConcreteClay Tile",
+  roofTypeMetal: "3 Metal",
+  roofTypeBuiltUp: "4 Built Up",
+  roofTypeMembrane: "5 Membrane",
+  roofTypeOther: "6 Other",
+
+  // --- Building Code Compliance ---
+  buildingCodeA: "buildingCodeA",
+  buildingCodeB: "buildingCodeB",
+  buildingCodeC: "buildingCodeC",
+
+  // --- Compliance Exceptions ---
+  shingleNoCompliance: "shingleNoCompliance",
+  clayNoCompliance: "clayNoCompliance",
+  metalNoCompliance: "metalNoCompliance",
+  builtUpNoCompliance: "builtUpNoCompliance",
+  membraneNoCompliance: "membraneNoCompliance",
+  otherNoCompliance: "otherNoCompliance",
+
+  // --- Roof Covering Methods ---
+  roofCoveringA: "roofCoveringA",
+  roofCoveringB: "roofCoveringB",
+  roofCoveringC: "roofCoveringC",
+  roofCoveringD: "roofCoveringD",
+
+  // --- Roof Deck Attachments ---
+  roofDeckA: "roofDeckA",
+  roofDeckB: "roofDeckB",
+  roofDeckC: "roofDeckC",
+  roofDeckD: "roofDeckD",
+  roofDeckE: "roofDeckE",
+  roofDeckF: "roofDeckF",
+  roofDeckG: "roofDeckG",
+
+  // --- Roof Geometry ---
+  roofGeometryA: "roofGeometryA",
+  roofGeometryB: "roofGeometryB",
+  roofGeometryC: "roofGeometryC",
+
+  // --- Roof to Wall Attachment ---
+  roofWallA: "roofWallA",
+  roofWallA1: "roofWallA1",
+  roofWallA2: "roofWallA2",
+  roofWallA3: "roofWallA3",
+  roofWallA4: "roofWallA4",
+  roofWallB: "roofWallB",
+  roofWallB1: "roofWallB1",
+  roofWallB2: "roofWallB2",
+  roofWallC: "roofWallC",
+  roofWallD: "roofWallD",
+  roofWallD1: "roofWallD1",
+  roofWallD2: "roofWallD2",
+  roofWallE: "roofWallE",
+  roofWallF: "roofWallF",
+  roofWallG: "roofWallG",
+  roofWallH: "roofWallH",
+
+  // --- Secondary Water Resistance (SWR) ---
+  swrA: "swrA",
+  swrB: "swrB",
+  swrC: "swrC",
+
+  // --- Opening Protection ---
+  oplA: "oplA",
+  oplA1: "oplA1",
+  oplA2: "oplA2",
+  oplA3: "oplA3",
+  oplB: "oplB",
+  oplB1: "oplB1",
+  oplB2: "oplB2",
+  oplB3: "oplB3",
+  oplC: "oplC",
+  oplC1: "oplC1",
+  oplC2: "oplC2",
+  oplC3: "oplC3",
+  oplN: "oplN",
+  oplN1: "oplN1",
+  oplN2: "oplN2",
+  oplN3: "oplN3",
+  oplX: "oplX",
+
+  // --- Inspector License Types ---
+  inspectorLicenseType1: "inspectorLicenseType1",
+  inspectorLicenseType2: "inspectorLicenseType2",
+  inspectorLicenseType3: "inspectorLicenseType3",
+  inspectorLicenseType4: "inspectorLicenseType4",
+  inspectorLicenseType5: "inspectorLicenseType5",
+  inspectorLicenseType6: "inspectorLicenseType6",
+};
